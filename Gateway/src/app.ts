155d789e8aa15537343dd8c76e721b827c65174e
Roman Ageev <roman.ageyev@devexpress.com>
2019-05-14 00:37:59 -0400

    add ts-node and nodemon

--- Gateway/src/app.ts
+++ Gateway/src/app.ts
@@ -1,1 +1,18 @@
-console.log("test X");
+import express from "express";
+import { Express, Request, Response } from "express";
+import bodyParser from "body-parser";
+
+
+const app: Express = express();
+
+app.use(bodyParser.urlencoded({
+    extended: false,
+}));
+
+app.use(bodyParser.json());
+
+app.get("/", (req: Request, res: Response) => res.send("Hello Gateway !!!"));
+
+const port: number = Number(process.env.PORT) || 3000;
+
+app.listen(port, () => console.log(`Gateway is listening on port ${port}...`));
