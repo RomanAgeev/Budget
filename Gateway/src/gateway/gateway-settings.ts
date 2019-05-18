8b21e6096f462527e2c3cc1909d8cba5cb9d6c49
Roman Ageev <roman.ageyev@devexpress.com>
2019-05-17 21:58:32 -0400

    support * property

--- Gateway/src/gateway/gateway-settings.ts
+++ Gateway/src/gateway/gateway-settings.ts
@@ -27,45 +27,49 @@ const readFileAsync = util.promisify(fs.readFile);
 
 export interface IVisitor {
     visitProp(name: string, obj: any): boolean;
-    visitText(value: string, propName?: string): boolean;
+    visitText(value: string): boolean;
 }
 
 export class Validator implements IVisitor {
     public visitProp(name: string, obj: any): boolean {
         return obj && obj[name];
     }
-    public visitText(value: string, propName: string): boolean {
+    public visitText(value: string): boolean {
         return typeof value === "string";
     }
 }
 
-type Check = (obj: any, propName?: string) => Reduce;
+type Check = (obj: any) => Reduce;
 type Reduce = (visitor: IVisitor, propName?: string) => boolean;
 
-const text = (value: any, propName?: string): Reduce =>
-    (visitor: IVisitor) => visitor.visitText(value, propName);
+const text = (value: any): Reduce =>
+    (visitor: IVisitor) => visitor.visitText(value);
 
-const list = (check: Check) => (value: any, propName?: string): Reduce => {
-    const reducers: Reduce[] = (value && value instanceof Array) ? value.map(it => check(it, propName)) : [];
+const list = (check: Check) => (value: any): Reduce => {
+    const reducers: Reduce[] = (value && value instanceof Array) ? value.map(it => check(it)) : [];
 
-    return (visitor: IVisitor) => reducers.every(reduce => reduce(visitor, propName)); // TODO: The same
+    return (visitor: IVisitor) => reducers.every(reduce => reduce(visitor)); // TODO: The same
 };
 
-const map = (checks: Check[]) => (value: any, propName?: string): Reduce => {
-    const reducers: Reduce[] = value ? checks.map(check => check(value, propName)) : [];
+const map = (checks: Check[]) => (value: any): Reduce => {
+    const reducers: Reduce[] = value ? checks.map(check => check(value)) : [];
 
-    return (visitor: IVisitor) => reducers.every(reduce => reduce(visitor, propName)); // TODO: The same
+    return (visitor: IVisitor) => reducers.every(reduce => reduce(visitor)); // TODO: The same
 };
 
 const prop = (name: string, checkValue: Check) => (obj: any): Reduce => {
-    const reducer: Reduce = checkValue(obj[name], name);
+    const names = name === "*" ? Object.getOwnPropertyNames(obj) : [name];
+    const reducers: Array<{ name: string, reducer: Reduce }> = names.map(it => ({
+        name: it,
+        reducer: checkValue(obj[it]),
+    }));
 
-    return (visitor: IVisitor) => visitor.visitProp(name, obj) && reducer(visitor, name);
+    return (visitor: IVisitor) => reducers.every(it => visitor.visitProp(it.name, obj) && it.reducer(visitor, it.name));
 };
 
 const parseSettings = map([
     prop("services", map([
-        prop("expenses", map([ // TODO: implement general solution like prop("*", ...)
+        prop("*", map([
             prop("host", text),
             prop("routes", list(map([
                 prop("public", text),
