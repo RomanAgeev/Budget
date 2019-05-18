8c9220ae427ac010819d729b9161c3e0ba804c19
Roman Ageev <roman.ageyev@devexpress.com>
2019-05-18 15:03:03 -0400

    json query - error handling spike

--- Gateway/src/gateway/gateway-settings.ts
+++ Gateway/src/gateway/gateway-settings.ts
@@ -16,8 +16,8 @@ const ensureSettings = async (settingsPath: string): Promise<void> => {
         const settingsYaml: string = await readFileAsync(settingsPath, "utf8");
         settings = yaml.safeLoad(settingsYaml);
 
-        const settingsReducer = parseSettings(settings);
-        if (settingsReducer) {
+        const settingsReducer = parseSettings(settings, []);
+        if (typeof settingsReducer === "function") {
             const results = settingsReducer(new QueryRequest("services/*/routes/*/public"));
             if (results.length > 0) {
                 console.log("public:" + JSON.stringify(results[0]));
@@ -32,8 +32,10 @@ const ensureSettings = async (settingsPath: string): Promise<void> => {
 
 const readFileAsync = util.promisify(fs.readFile);
 
-type Check = (obj: any) => Query | null;
+type Check = (obj: any, position: string[]) => Query | ParseError[];
+type ParseError = { path: string, message: string };
 type Query = (request: QueryRequest) => IQueryResult[];
+// type QueryPredicate = (value: any) => boolean;
 
 class QueryRequest {
     constructor(pathStr: string) {
@@ -85,7 +87,7 @@ interface IQueryResult {
     readonly value: any;
 }
 
-const text = (value: any): Query | null => {
+const text = (value: any, position: string[]): Query | ParseError[] => {
     if (typeof value === "string") {
         return (request: QueryRequest): IQueryResult[] => {
             if (request.found) {
@@ -97,15 +99,24 @@ const text = (value: any): Query | null => {
             return [];
         };
     }
-    return null;
+    return [{
+        path: position.join("/"),
+        message: "string value is expected",
+    }];
 };
 
-const list = (check: Check) => (value: any): Query | null => {
+const list = (check: Check) => (value: any, position: string[]): Query | ParseError[] => {
     if (value instanceof Array) {
-        const queries = value.map(it => check(it));
-        if (queries.some(it => it === null)) {
-            return null;
+        const queries = value.map((it, index: number) => check(it, position.concat(index.toString())));
+
+        const errors = queries.filter(it => Array.isArray(it)) as ParseError[][];
+        if (errors.length > 0) {
+            return errors.reduce((error: ParseError[], acc: ParseError[]) => acc.concat(error), []);
         }
+        // if (queries.some(it => it === null)) {
+        //     return null;
+        // }
+
         return (request: QueryRequest): IQueryResult[] => {
             if (request.found) {
                 return [{
@@ -113,7 +124,7 @@ const list = (check: Check) => (value: any): Query | null => {
                     value,
                 }];
             }
-            return queries.map((it, index: number) => {
+            return (queries as Query[]).map((it, index: number) => {
                 if (request.goDown(index.toString())) {
                     const result = it!(request);
                     request.goUp();
@@ -124,14 +135,23 @@ const list = (check: Check) => (value: any): Query | null => {
             .reduce((results: IQueryResult[], acc: IQueryResult[]) => acc.concat(results), []);
         };
     }
-    return null;
+    return [{
+        path: position.join("/"),
+        message: "array is expected",
+    }];
 };
 
-const map = (checks: Check[]) => (value: any): Query | null => {
-    const queries = value ? checks.map(check => check(value)) : [];
-    if (queries.some(it => it === null)) {
-        return null;
+const map = (checks: Check[]) => (value: any, position: string[]): Query | ParseError[] => {
+    const queries = value ? checks.map(check => check(value, position)) : [];
+
+    const errors = queries.filter(it => Array.isArray(it)) as ParseError[][];
+    if (errors.length > 0) {
+        return errors.reduce((error: ParseError[], acc: ParseError[]) => acc.concat(error), []);
     }
+
+    // if (queries.some(it => it === null)) {
+    //     return null;
+    // }
 
     return (request: QueryRequest): IQueryResult[] => {
         if (request.found) {
@@ -140,22 +160,31 @@ const map = (checks: Check[]) => (value: any): Query | null => {
                 value,
             }];
         }
-        return queries
+        return (queries as Query[])
             .map(it => it!(request))
             .reduce((results: IQueryResult[], acc: IQueryResult[]) => acc.concat(results), []);
     };
 };
 
-const prop = (name: string, checkValue: Check) => (obj: any): Query | null => {
+const prop = (name: string, checkValue: Check) => (obj: any, position: string[]): Query | ParseError[] => {
     const names = name === "*" ? Object.getOwnPropertyNames(obj) : [name];
 
-    const queries = names.map(it => obj[it] ? checkValue(obj[it]) : null);
-    if (queries.some(it => it === null)) {
-        return null;
+    const queries = names.map(it => obj[it] ? checkValue(obj[it], position.concat(it)) : [{
+        path: position.join("/"),
+        message: `property ${it} is expected`,
+    }]);
+
+    const errors = queries.filter(it => Array.isArray(it)) as ParseError[][];
+    if (errors.length > 0) {
+        return errors.reduce((error: ParseError[], acc: ParseError[]) => acc.concat(error), []);
     }
 
+    // if (queries.some(it => it === null)) {
+    //     return null;
+    // }
+
     return (request: QueryRequest): IQueryResult[] => {
-        return queries.map((it, index: number) => {
+        return (queries as Query[]).map((it, index: number) => {
             if (request.goDown(names[index])) {
                 const result = it!(request);
                 request.goUp();
