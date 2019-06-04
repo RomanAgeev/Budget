f357016ff52328f5f2f9b4216f564a85aedc6015
Roman Ageev <roman.ageyev@devexpress.com>
2019-06-04 00:20:14 -0400

    first test with sinon

---
+++ Gateway/test/sign-in.test.ts
@@ -0,0 +1,65 @@
+import { describe, it } from "mocha";
+import { expect } from "chai";
+import sinon from "sinon";
+import jwt from "jsonwebtoken";
+import { UserModel } from "../src/user-model";
+import { Storage } from "../src/storage";
+import { createHash } from "../src/password";
+import { signIn } from "../src/sign-in";
+
+describe("sign in", () => {
+    it("first test", async () => {
+        const salt = "testsalt";
+        const password = "testpass";
+
+        const hash = createHash(password, salt);
+
+        const user: UserModel = {
+            username: "testuser",
+            salt,
+            hash,
+            admin: false,
+            enabled: true,
+        };
+
+        const fakeStorage: Storage = {
+            getUsers: sinon.fake(),
+            getUser: sinon.fake(),
+            addUser: sinon.fake(),
+            updateUser: sinon.fake(),
+            deleteUser: sinon.fake(),
+            close: sinon.fake(),
+        };
+
+        sinon.replace(fakeStorage, "getUser", sinon.fake.resolves(user));
+
+        const signin = signIn("testSecret", fakeStorage);
+
+        const req: any = {
+            body: {
+                username: "testuser",
+                password,
+            },
+        };
+
+        const res: any = {
+            status: sinon.fake(),
+            sendStatus: sinon.fake(),
+            send: sinon.fake(),
+            json: sinon.fake(),
+        };
+
+        sinon.stub(jwt, "sign").callsFake((payload, secret, options) => payload);
+
+        await signin(req, res);
+
+        sinon.assert.calledWith(res.json, {
+            token: {
+                username: "testuser",
+                enabled: true,
+                admin: false,
+            },
+        });
+        sinon.assert.calledOnce(res.json);
+    });
+});
